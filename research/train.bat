71e9e917d2f83e2d362c4a706e106eb3736d3fd4
Kenny Liao <irvingliao@gmail.com>
2019-07-03 01:38:38 -0700

    Setup for local train

--- research/train.bat
+++ research/train.bat
@@ -1,4 +1,6 @@
 set BASE_PATH=D:\Code\Self-Driving-Car\models\research\object_detection
-set PIPELINE_CONFIG_PATH=%BASE_PATH%\model\ssd_mobilenet_v1_coco_11_06_2017\pipeline.config
-set EXPORT_DIR=%BASE_PATH%\export_models\new
-python object_detection/model_main.py --pipeline_config_path=%PIPELINE_CONFIG_PATH% --model_dir=%EXPORT_DIR% --num_train_steps=40000 --sample_1_of_n_eval_examples=1 --alsologtostderr
+set PIPELINE_CONFIG_PATH=%BASE_PATH%\local_train.config
+set EXPORT_DIR=%BASE_PATH%\export_models\02
+set TRAIN_DIR=%BASE_PATH%\train\02
+REM python object_detection/model_main.py --pipeline_config_path=%PIPELINE_CONFIG_PATH% --model_dir=%EXPORT_DIR% --num_train_steps=20000 --sample_1_of_n_eval_examples=1 --alsologtostderr
+python object_detection/train.py --logtostderr --pipeline_config_path=%PIPELINE_CONFIG_PATH% --train_dir=%TRAIN_DIR%
